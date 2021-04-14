551f1457c59ac4ae721135a7b97f863590b9d173
Daniel Botea <danielmbotea@gmail.com>
2021-04-15 01:21:57 +0300

    add blog to fe

--- pages/index.js
+++ pages/index.js
@@ -3,23 +3,44 @@ import PageLayout from "components/PageLayout";
 import AuthorIntro from "components/AuthorIntro"
 import CardListItem from "components/CardListItem";
 import CardItem from "components/CardItem";
+import {getAllBlogs} from "../lib/api";
 
-export default function Home() {
+export default function Home({blogs}) {
 
   return (
       <PageLayout className='home'>
         <AuthorIntro />
         <hr/>
-        <Row className="mb-5">
-          <Col md="10">
-            <CardListItem />
-          </Col>
 
-          <Col md="4">
-            <CardItem />
-          </Col>
+        <Row className="mb-5">
+          {/*<Col md="10">*/}
+          {/*  <CardListItem />*/}
+          {/*</Col>*/}
+          {
+            blogs.map( blog =>
+            <Col key={blog._id} md="4">
+              <CardItem
+                title={blog.title}
+                subtitle={blog.subtitle}
+              />
+            </Col>
+            )
+          }
         </Row>
       </PageLayout >
   )
+}
+
+// this function is called during the build (build time)
+// Provides props to your page
+// it will create static page
+export async function getStaticProps() {
+  console.log('Calling getStaticProps')
+  const blogs = await getAllBlogs();
+  return {
+    props: {
+      blogs
+    }
+  }
 }
 
